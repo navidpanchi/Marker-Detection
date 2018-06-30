b25faa1fb3ac3a4ea71ea4b6019d512ff620b910
navidpanchi <navidpanchi@gmail.com>
2018-07-01 00:29:49 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 echo "installing dependencies : pip, tensorflow(cpu), keras"
 
-sudo apt install pip
-pip install tensorflow
-pip install keras
+sudo apt-get install pip
+pip install --user tensorflow
+pip install --user keras
 pip install cv2
 pip install numpy
